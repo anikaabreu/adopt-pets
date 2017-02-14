2cebf5b57a20c2c2e3f92c9bd26c29c36e31e496
Anika Abreu <anika.abreu9@gmail.com>
2017-02-13 21:24:44 -0500

    Update pets.js

--- app/assets/javascripts/pets.js
+++ app/assets/javascripts/pets.js
@@ -1,33 +1,20 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-    // $(document).ready(function(){
-    //   $('.carousel').slick({
-    //     setting-name: setting-value
-    //   });
-    // });
+  
+$(document).ready(function(){
+    var link = $("#demo01");
 
-    // Get the modal
-    var modal = document.getElementById('myModal');
-
-    // Get the button that opens the modal
-    var btn = document.getElementById("myBtn");
-
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
-
-    $(function() {
-      modal.style.display = "block";
-    })
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
+    link.animatedModal();
+    link.trigger('click');
+    $("#nav-mobile").html($("#nav-main").html());
+    $("#nav-trigger span").click(function(){
+        if ($("nav#nav-mobile ul").hasClass("expanded")) {
+            $("nav#nav-mobile ul.expanded").removeClass("expanded").slideUp(250);
+            $(this).removeClass("open");
+        } else {
+            $("nav#nav-mobile ul").addClass("expanded").slideDown(250);
+            $(this).addClass("open");
         }
-    };
+    });
+});
