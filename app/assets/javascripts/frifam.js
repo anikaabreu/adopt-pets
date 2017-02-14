7c46ddf0db242ba0fda47edc9b4ab5e894109a2a
Anika Abreu <anika.abreu9@gmail.com>
2017-02-14 18:54:57 -0500

    styling changes

--- app/assets/javascripts/frifam.js
+++ app/assets/javascripts/frifam.js
@@ -1,6 +1,3 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
-
 
 $(document).ready(function(){
     var link = $("#demo01");
@@ -18,6 +15,10 @@ $(document).ready(function(){
         }
     });
 });
+
+
+
+
 
 
 
