bb7fc9e871275ac867947b37be0b4d83d4d12e6d
AN1CO <skye.anico@gmail.com>
2017-02-20 18:54:02 -0500

    added login page, deleted json stuff

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,24 +1,16 @@
 class FavoritesController < ApplicationController
   def like
     pet = Pet.find(params[:pet_id])
-    # user = User.find(session[:user_id])
     favorite = Favorite.create(pet: pet, user: current_user)
-    # if !user.favorites.nil?
-    #   redirect_to '/'
-    # else
-    #   redirect_to '/signup'
-    # end
     render json: Pet.random
   end
 
   def index
-
     if session[:user_id].nil?
       redirect_to '/signup'
     else
       # user = User.find(session[:user_id])
       @favorites = current_user.favorites
     end
-
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,16 @@ Rails.application.routes.draw do
   resources :pets
   resources :users
   resources :favorites
-  
+
   root "pets#show"
   get '/randompet' => 'pets#random'
   get '/favoritepet/:pet_id' => 'favorites#like', as: 'favorite_pet'
-  get '/signup' => 'users#new'
   get '/favorites' => 'favorites#index'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
